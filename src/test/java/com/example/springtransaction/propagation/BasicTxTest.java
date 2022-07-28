28660e5b4827059c5709e273a814fb6ff9df3757
hyejin1231 <phj981231@naver.com>
2022-07-28 10:21:50 +0900

    20220728 스프링 트랜잭션 전파2 - 트랜잭션 두 번 사용

--- src/test/java/com/example/springtransaction/propagation/BasicTxTest.java
+++ src/test/java/com/example/springtransaction/propagation/BasicTxTest.java
@@ -13,6 +13,10 @@ import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
 
 import javax.sql.DataSource;
 
+/**
+ * 트랜잭션 전파
+ * 트랜잭션이 이미 진행중인데 여기에 추가로 트랜잭션을 수행할때 어떻게 동작할지 결정하는 것
+ */
 @Slf4j
 @SpringBootTest
 public class BasicTxTest {
@@ -43,6 +47,40 @@ public class BasicTxTest {
         log.info("트랜잭션 롤백 시작");
         txManager.rollback(status);
         log.info("트랜잭션 롤백 완료");
+    }
+
+    /**
+     * 트랜잭션 두번 사용
+     * -> 트랜잭션1 완전히 끝나면 트랜잭션2 수행
+     * 트랜잭션이 각각 수행되면서 사용되는 DB 커넥션도 각각 다르다.
+     */
+    @Test
+    void double_commit() {
+        log.info("트랜잭션1 시작");
+        TransactionStatus transaction1 = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜잭션1 커밋");
+        txManager.commit(transaction1);
+
+        log.info("트랜잭션2 시작");
+        TransactionStatus transaction2 = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜잭션2 커밋");
+        txManager.commit(transaction2);
+    }
+
+    /**
+     * 트랜잭션1은 커밋, 트랜잭션2는 롤백
+     */
+    @Test
+    void double_commit_rollback() {
+        log.info("트랜잭션1 시작");
+        TransactionStatus transaction1 = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜잭션1 커밋");
+        txManager.commit(transaction1);
+
+        log.info("트랜잭션2 시작");
+        TransactionStatus transaction2 = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜잭션2 커밋");
+        txManager.rollback(transaction2);
     }
 
 }
